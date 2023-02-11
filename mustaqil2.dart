b2b7c08085428a79c396592102520f0d0622dae1
network7277 <husanjurayev7277@gmail.com>
2023-02-11 17:16:15 +0500

    telefon rasmi

---
+++ mustaqil2.dart
@@ -0,0 +1,20 @@
+void main(){
+    print (" ==========   ==========");
+    print ("|           *           |");
+    print ("|   22:00     24:00     |");
+    print ("|  Toshkent   Maskva    ||");
+    print ("|                       ||");
+    print ("|                       |");
+    print ("|                       ||");
+    print ("|  [ ]  [ ]  [ ]  [ ]   |");
+    print ("|                       |");
+    print ("|    [1]  [2]   [3]     |");
+    print ("|    [4]  [5]   [6]     |");
+    print ("|    [7]  [8]   [9]     |");
+    print ("|         [0]           |");
+    print ("|                       |");
+    print ("|               [OK]    |");
+    print ("|  []        O      <   |");
+    print (" ===========  ==========");
+    print ("_________ CODESCHOOL UZ ________");
+}
