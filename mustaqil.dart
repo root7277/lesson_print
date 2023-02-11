ce68e7122753485eceb02e078e083aab0250a924
network7277 <husanjurayev7277@gmail.com>
2023-02-11 17:08:28 +0500

    kom rasm1

---
+++ mustaqil.dart
@@ -0,0 +1,21 @@
+void main(){
+    print ("      ========================================");
+    print ("    ||                                        ||");
+    print ("    ||                Husan                   ||");
+    print ("    ||                      Jurayev           ||");
+    print ("    ||                                        ||");
+    print ("    ||     @codesckooluz                      ||");
+    print ("    ||     My teacher:                        ||");
+    print ("    ||               Zariifjon                ||");
+    print ("    ||                        Naxalov         ||");
+    print ("                       ( . )                 ");
+    print ("      ========================================");
+    print ("                    /__________\\                ");
+
+    print ("     --------------------------------------------");
+    print ("    /  1   2   3   4   5   6   7   8   9   0    /");
+    print ("   /  Q   W   E   R   T   Y   U   I   O    P   /");
+    print ("  / A   S   D   F   G   H   J   K   L    :    /");
+    print (" / Z   X   C   V   B   N   M   <   >    ?    /");
+    print ("--------------------------------------------");
+}
